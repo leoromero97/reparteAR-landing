0c08aad2dccccd00497d0da8e3332ff0a6f5d9a0
leoromero97 <leonardogerbacio@gmail.com>
2023-05-20 12:04:21 -0300

    Fixed spacing in Services component

--- app/components/Sections/Services/index.tsx
+++ app/components/Sections/Services/index.tsx
@@ -14,8 +14,8 @@ function Services({ className }: IServicesProps) {
   const isMobile = useMobileDetect();
 
   return (
-    <section className="flex flex-col items-center w-full bg-skyblue-100">
-      <div className={clsx('flex flex-col max-w-7xl px-8 w-full', className, isMobile ? 'items-center py-10' : 'py-40')}>
+    <section className="flex flex-col items-center w-full bg-skyblue-100 md:py-20">
+      <div className={clsx('flex flex-col max-w-7xl px-8 w-full py-10', className, isMobile && 'items-center')}>
         <h3 className={clsx(isMobile ? 'text-subtitle font-bold text-center' : 'text-h3 font-bold mb-6')}>{texts.services.titleServices}</h3>
         <div className={clsx('flex w-full py-6', isMobile ? 'overflow-scroll gap-4' : 'flex-wrap gap-10')}>
           {services.map(({ icon, id, title }) => (
