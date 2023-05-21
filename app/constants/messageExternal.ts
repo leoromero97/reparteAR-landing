7c4c483efc24427d2e7b805164dd0a6b64fffb94
leoromero97 <leonardogerbacio@gmail.com>
2023-05-21 17:57:22 -0300

    Leo Romero - feature component CTA main - Refactored Home page to CTA main in mobile view - Refactored MessageTech, and Sidebar components - Created messageExternal and externalLink constants

---
+++ app/constants/messageExternal.ts
@@ -0,0 +1,15 @@
+export const messageExternal = `Hola reparteAR! Quiero realizar un pedido. Los datos son: \n\n
+- Dirección de salida: \n
+- Barrio de salida: \n
+- Dirección de destino: \n
+- Barrio de destino: \n
+- Timbre destinatario: \n
+- Teléfono destinatario: \n
+- Nombre/apellido destinatario: \n
+- Franja horaria de entrega: \n
+- Cantidad de paquetes: \n
+- Tamaño del paquete: \n
+- Medida y peso aproximado: \n
+- Observaciones: \n`;
+
+export const externalLink = `https://wa.me/541164392829?text=${messageExternal}`
