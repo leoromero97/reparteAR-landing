25d2f59adf0155057c293f92f57ed0b61d3de26a
leoromero97 <leonardogerbacio@gmail.com>
2023-08-02 15:35:30 -0300

    Bug Navbar component - Solved fixes in some texts constant - Solved fixe in HowWeWork section component - Solved bug in NavMobile, NavbDesktop and Navbar components - Changed and solved fixes in page.tsx

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,25 +1,22 @@
 import { useState } from 'react';
-import useMobileDetect from '@/hooks/useMobileDetected';
 
 import NavDesktop from './NavDesktop';
 import NavMobile from './NavMobile';
 
-type INavbar = {
-  isMobile: boolean;
-}
-
-function Navbar({ isMobile }: INavbar) {
+function Navbar() {
   const [menuOpened, setMenuOpened] = useState(false);
   const onOpenMenu = () => {
     setMenuOpened(!menuOpened);
   };
 
   return (
-    isMobile ? (
-      <NavMobile menuOpened={menuOpened} onClick={onOpenMenu} />
-    ) : (
+    <>
+      <NavMobile
+        menuOpened={menuOpened}
+        onClick={onOpenMenu}
+      />
       <NavDesktop />
-    )
+    </>
   );
 }
 
